c58a3d7b61e0f46b0bf840e3ed22198c07b7f4e8
Gülşen <gulsenk48@hotmail.com>
2024-09-30 19:41:57 +0300

    First_app

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstProject
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gülşen Kavak\nPatika Bootcampi terci etme sebebim, çalışma tarzları sayesinde yazılıma başlangıç yapabileceğim başarılı ve ilgili bir oluşum olduğunu düşünmem.");
+           
+        }
+    }
+}
